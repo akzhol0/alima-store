d7fe35701443adcefb7ee476c650c4e0cff75198
akzhol0 <ak.akzhol1603@gmail.com>
2025-03-18 21:35:56 +0500

    debug

--- src/components/home-page/HomePageUnitPost.tsx
+++ src/components/home-page/HomePageUnitPost.tsx
@@ -8,10 +8,10 @@ type HomePageUnitPostProps = {
 function HomePageUnitPost({ title, small1, small2, img }: HomePageUnitPostProps) {
   return (
     <div className="w-full my-[80px] flex flex-wrap justify-center">
-      <div className="w-[100%] md:w-[50%] flex gap-5 flex-col justify-center items-center">
-        <strong className='w-full text-[30px] text-start'>{title}</strong>
-        <small className='text-[18px] text-start text-gray-500'>{small1}</small>
-        <small className='w-full hidden md:block text-[18px] text-start text-gray-500'>{small2}</small>
+      <div className="w-[100%] md:w-[50%] flex flex-col justify-center items-start">
+        <strong className='w-full text-[30px] text-start mb-6'>{title}</strong>
+        <small className='text-sm md:text-lg text-start text-gray-500'>{small1}</small>
+        <small className='w-full text-sm md:text-lg text-start text-gray-500'>{small2}</small>
       </div>
       <div className="w-[100%] md:w-[50%] flex flex-col justify-center items-center">
         <img src={img} alt="unit picture" />
